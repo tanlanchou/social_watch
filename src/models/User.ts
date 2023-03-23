f8bd66af86ced79ebe03704cbaffe6aea3a93b0a
tommy <tlstommy@qq.com>
2023-03-23 23:44:50 +0800

    fix bug

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,29 +1,85 @@
+// **** Variables **** //
 
-// Remove the export statement for UserClass
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
-  createdAt: Date;
-  updatedAt: Date;
+const INVALID_CONSTRUCTOR_PARAM = 'nameOrObj arg must a string or an ' + 
+  'object with the appropriate user keys.';
+
+export enum UserRoles {
+  Standard,
+  Admin,
 }
 
-class UserClass implements User {
+
+// **** Types **** //
+
+export interface IUser {
   id: number;
   name: string;
   email: string;
-  password: string;
-  createdAt: Date;
-  updatedAt: Date;
+  pwdHash?: string;
+  role?: UserRoles;
+}
 
-  constructor(id: number, name: string, email: string, password: string) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.createdAt = new Date();
-    this.updatedAt = new Date();
+export interface ISessionUser {
+  id: number;
+  email: string;
+  name: string;
+  role: IUser['role'];
+}
+
+
+// **** Functions **** //
+
+/**
+ * Create new User.
+ */
+function new_(
+  name?: string,
+  email?: string,
+  role?: UserRoles,
+  pwdHash?: string,
+  id?: number, // id last cause usually set by db
+): IUser {
+  return {
+    id: (id ?? -1),
+    name: (name ?? ''),
+    email: (email ?? ''),
+    role: (role ?? UserRoles.Standard),
+    pwdHash: (pwdHash ?? ''),
+  };
+}
+
+/**
+ * Get user instance from object.
+ */
+function from(param: object): IUser {
+  // Check is user
+  if (!isUser(param)) {
+    throw new Error(INVALID_CONSTRUCTOR_PARAM);
   }
+  // Get user instance
+  const p = param as IUser;
+  return new_(p.name, p.email, p.role, p.pwdHash, p.id);
 }
-export { User, UserClass };
+
+/**
+ * See if the param meets criteria to be a user.
+ */
+function isUser(arg: unknown): boolean {
+  return (
+    !!arg &&
+    typeof arg === 'object' &&
+    'id' in arg &&
+    'email' in arg &&
+    'name' in arg &&
+    'role' in arg
+  );
+}
+
+
+// **** Export default **** //
+
+export default {
+  new: new_,
+  from,
+  isUser,
+} as const;
